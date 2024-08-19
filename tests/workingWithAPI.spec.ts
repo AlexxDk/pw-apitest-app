75a139e240212e0b142f96e14eae75ec314ba18c
AlexxDk <dkalexandr@gmail.com>
2024-08-19 19:14:23 +0300

    mocking api request

--- tests/workingWithAPI.spec.ts
+++ tests/workingWithAPI.spec.ts
@@ -1,9 +1,22 @@
 import { test, expect } from '@playwright/test';
 
 test.beforeEach(async ({ page }) => {
+  await page.route('https://conduit-api.bondaracademy.com/api/tags', async route => {
+    const tags = {
+      "tags": [
+        "YouTube",
+        "Zoom",
+      ]
+    }
+    await route.fulfill({
+      body: JSON.stringify(tags)
+    })
+  })
+
   await page.goto('https://conduit.bondaracademy.com/')
 })
 
 test('has title', async ({ page }) => {
   await expect(page.locator('.navbar-brand')).toHaveText('conduit');
+  await expect(page.locator('.tag-list')).toBeVisible()
 })
